f8f043ddcee8985a7d7be7cd0c4efb85be2de3f0
AadrikaK <aadrikakhare99@gmail.com>
2021-07-29 21:38:51 +0530

    day9

--- day9_assignment-29july/mobile1.py
+++
@@ -1,9 +0,0 @@
-import re
-mobile=input("enter a mobile number")
-
-validation=re.search("^[6-9]\d{9}$",mobile)#[0-9]ord{9}
-
-if validation:
-    print("accepted")
-else:
-    print("rejected")
--- day9_assignment-29july/regular.py
+++
@@ -1,7 +0,0 @@
-import re
-textinput=input("enter a text")
-val=re.search("^the.*hello$",textinput)
-if val:
-    print("accepted")
-else:
-    print("rejected")
